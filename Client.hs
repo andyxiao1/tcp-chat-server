683df65de5e625bcdf18518b57ba9e03dd52997b
Prachi Nawathe <pnawathe@usc.edu>
2020-12-05 19:17:49 -0800

    completed client, still need to set up client threads in server

--- Client.hs
+++ Client.hs
@@ -1,4 +1,4 @@
-module Client (local, client, send) where
+module Client where
 
 import Control.Applicative (Alternative (..), liftA3)
 import Control.Monad ()
@@ -10,6 +10,8 @@ import qualified Data.Maybe
 
 -- import qualified Data.Time.Clock as Clock
 -- import Network.Info as Info
+
+import Data.Text (pack, strip, unpack)
 import Network.Socket hiding (send)
 import System.IO
   ( BufferMode (BlockBuffering),
@@ -31,13 +33,11 @@ client = do
   -- run with command line arguments of: name, port
   putStrLn "what is your name?"
   name <- getLine
-  putStrLn "What port are you publishing over?"
+  putStrLn "What port do you want to connect to?"
   port <- getLine
-  putStrLn "What is your VPN IP address?"
-  ip <- getLine
   putStrLn "What is the IP of the server you are connecting to?"
-  sip <- getLine
-  clientF name port ip sip
+  ip <- getLine
+  clientF name port ip
 
 -- print connection results
 type Username = String
@@ -46,23 +46,70 @@ type Port = String
 
 type IPv4 = String
 
+type RoomName = String
+
+type User = String
+
+type MessageContent = String
+
+data Action
+  = GetAllRooms
+  | GetAllRoomMessages RoomName
+  | CreateRoom RoomName
+  | -- | DeleteRoom RoomName
+    AddUserToRoom Username RoomName
+  | SwitchUserBetweenRooms Username RoomName RoomName
+  | SendRoomMessage Username RoomName MessageContent
+  | Quit
+  deriving (Eq, Show)
+
 -- loop client
 
-clientF :: Username -> Port -> IPv4 -> IPv4 -> IO Handle
-clientF u port h ip = do
+clientF :: Username -> Port -> IPv4 -> IO Handle
+clientF u port ip = do
   (sa : _) <- getAddrInfo Nothing (Just ip) (Just port)
   sock <- socket (addrFamily sa) Stream defaultProtocol
   connect sock (addrAddress sa)
   handle <- socketToHandle sock WriteMode
   hSetBuffering handle (BlockBuffering Nothing)
-  return handle
-
--- | IP address of the local host
-local :: HostName
-local = "127.0.0.1"
+  -- print out all rooms
+  -- ask user to join room
+  let rm = "room1"
+  -- check if the room they tried to join exists
+  run u rm handle
+  where
+    run u rm handle = do
+      putStr "Client> "
+      msg <- getLine -- don't want this to be a blocking statement
+      let action = createAction u rm msg
+      -- if action is `Quit` end the loop
+      send handle (show action)
+      -- if current action is `SwitchUserBetweenRooms` then check if nw request worked and update room name in recursive call
+      run u rm handle
 
 -- | send a message to the server
 send :: Handle -> String -> IO ()
 send h c = do
   hPutStrLn h c
   hFlush h
+
+createAction :: User -> RoomName -> MessageContent -> Action
+createAction u r m =
+  case unpack (strip (pack m)) of
+    ':' : command -> case command of
+      -- create room
+      'n' : ' ' : rm -> CreateRoom rm
+      -- delete room
+      -- 'd' : ' ' : rm -> DeleteRoom rm
+      -- join room
+      'u' : ' ' : rm -> AddUserToRoom u rm
+      -- switch room
+      's' : ' ' : rm -> SwitchUserBetweenRooms u r rm
+      -- see all rooms
+      "g" -> GetAllRooms
+      -- quit
+      "q" -> Quit
+      -- unknown command
+      _ -> undefined -- putStrLn "?" >> go store
+      -- send message to room
+    msg -> SendRoomMessage u r msg
--- Server.hs
+++ Server.hs
@@ -1,121 +1,134 @@
 module Server where
 
 import Control.Applicative (Alternative (..), liftA3)
-import Control.Monad ()
 -- import Data.ProtocolBuffers
 
+import Control.Concurrent
+import Control.Monad ()
 import Data.IORef
+import Data.List (delete)
 import Data.Map (Map)
 import qualified Data.Map as Map
 import qualified Data.Maybe
 import qualified Data.Time.Clock as Clock
 import GHC.Generics as Gen
 import Network.Socket
+import ParserCombinators (Parser)
+import qualified ParserCombinators as P
+import State (State)
+import qualified State as S
 import Text.PrettyPrint (Doc)
 import qualified Text.PrettyPrint as PP
+
+-- -----------------------------
+-- -- Type Definitions (Model)
+-- -----------------------------
+
+-- -- data ThreadMessage = TM Message deriving (Eq, Show)
+
+-- newtype Space = S [Room]
+
+-- data RoomMessage = RM Message Thread
+
+-- data Room = L [RoomMessage] RoomName [Members]
+
+-- data Thread = E | TL [Message] Message
+
+-- data Message = M Sender Content Timestamp
+
+-- -- data SerializedMessage = {}
+
+-- type Content = String
+
+-- type Timestamp = Clock.UTCTime
+
+-- type Sender = String
+
+-- type RoomName = String
+
+-- type Port = Int
+
+-- newtype Members = Mem [Sender]
+
+type IPv4 = String
+
+-- type Store = Map RoomName Room
+
+-- -- is it better to store users in rooms to avoid map lookup?
+-- -- newtype Users = Map Sender (IPv4, Port)
 
 -----------------------------
 -- Type Definitions (Model)
 -----------------------------
 
--- data ThreadMessage = TM Message deriving (Eq, Show)
-
-newtype Space = S [Room]
-
-data RoomMessage = RM Message Thread
-
-data Room = L [RoomMessage] RoomName [Members]
-
-data Thread = E | TL [Message] Message
-
-data Message = M Sender Content Timestamp
-
--- data SerializedMessage = {}
-
-type Content = String
-
-type Timestamp = Clock.UTCTime
-
-type Sender = String
-
 type RoomName = String
 
-type Port = Int
+type User = String
 
-newtype Members = Mem [Sender]
+type Username = String
 
-type IPv4 = String
+type MessageContent = String
 
--- is it better to store users in rooms to avoid map lookup?
--- newtype Users = Map Sender (IPv4, Port)
+type Store = Map RoomName Room
+
+data Room = R {name :: String, messages :: [Message], users :: [User]}
+
+data Message = M {sender :: User, content :: String} deriving (Show)
+
+emptyStore :: Store
+emptyStore = Map.empty
 
 -----------------------------
 -- Function Declarations
 -----------------------------
 
--- minimize io usage
+getAllRooms :: Store -> [RoomName]
+getAllRooms = Map.keys
 
-type MVar a = IORef (Maybe a)
+getAllRoomMessages :: Store -> RoomName -> [Message]
+getAllRoomMessages store room = case Map.lookup room store of
+  Nothing -> []
+  Just (R _ messages _) -> messages
 
-data Action
-  = Atom (IO Action) -- an atomic computation, returning a new action
-  | Fork Action Action -- create a new thread
-  | Stop -- terminate this thread
+createRoom :: RoomName -> State Store ()
+createRoom r = do
+  store <- S.get
+  S.put (Map.insert r (R r [] []) store)
 
-writeAction :: String -> Action
-writeAction = Action
+deleteRoom :: RoomName -> State Store ()
+deleteRoom r = do
+  store <- S.get
+  S.put (Map.delete r store)
 
-prog :: Action
-prog = Fork (writeAction "Hello\n") (writeAction "CIS 552\n")
+addUserToRoom :: User -> RoomName -> State Store ()
+addUserToRoom usr room = do
+  store <- S.get
+  S.put (Map.adjust updateRoom room store)
+  where
+    updateRoom rm@(R name messages users) =
+      if usr `elem` users
+        then rm
+        else R name messages (usr : users)
 
-newtype C a = C {runC :: (a -> Action) -> Action}
+removeUserFromRoom :: User -> RoomName -> State Store ()
+removeUserFromRoom usr room = do
+  store <- S.get
+  S.put (Map.adjust (\(R name messages users) -> R name messages (delete usr users)) room store)
 
-atom :: IO a -> Server.C a
-atom x = C $ \k -> k x
+switchUserBetweenRooms :: User -> RoomName -> RoomName -> State Store ()
+switchUserBetweenRooms usr r1 r2 = do
+  removeUserFromRoom usr r1
+  addUserToRoom usr r2
 
-class Monad m => MVarMonad m where
-  newMVar :: m (MVar a)
-  writeMVar :: MVar a -> a -> m ()
-  takeMVar :: MVar a -> m (Maybe a)
-
-instance MVarMonad IO where
-  newMVar = newIORef Nothing
-  writeMVar v a = writeIORef v (Just a)
-  takeMVar a = readIORef a
-
--- instance MVarMonad C where
---   newMVar = atom newMVar
---   writeMVar v a = atom (writeMVar v a)
---   takeMVar v = atom (takeMVar v)
-
--- listen :: IO ()
--- listen = undefined
-
-recvMsg :: IPv4 -> Port -> String -> IO ()
-recvMsg = undefined
-
-connectSenderToRoom :: IPv4 -> Port -> RoomName -> IO ()
-connectSenderToRoom = undefined
-
-removeSenderFromRoom :: Sender -> RoomName -> IO ()
-removeSenderFromRoom = undefined
-
-switchSenderBWRooms :: Sender -> RoomName -> RoomName -> IO ()
-switchSenderBWRooms s r1 r2 = do
-  removeSenderFromRoom s r1
-  connectSenderToRoom s r2
-
-getRooms :: Space
-getRooms = undefined
-
-sendMsgRm :: Sender -> String -> Room -> IO ()
-sendMsgRm = undefined
-
-sendMsgThread :: Sender -> String -> Thread -> IO ()
-sendMsgThread = undefined
-
-sendMsg :: Sender -> String -> IO ()
-sendMsg = undefined
+sendRoomMessage :: User -> RoomName -> MessageContent -> State Store ()
+sendRoomMessage usr room msg = do
+  store <- S.get
+  S.put (Map.adjust updateRoom room store)
+  where
+    updateRoom rm@(R name messages users) =
+      if usr `notElem` users
+        then rm
+        else R name (M usr msg : messages) users
 
 -----------------------------
 -- Test Cases
@@ -134,25 +147,71 @@ sendMsg = undefined
 -- case head rms of
 --   RM msg th -> msg == str
 
+clientThread :: Socket -> IO a
+clientThread sock = undefined
+
+network :: IPv4 -> IO ()
+network ip = do
+  withSocketsDo $ do
+    putStrLn "Opening a socket."
+    addr <- resolve
+    sock <- open addr
+    (conn, _peer) <- accept sock
+    putStrLn "Connected to socket."
+    --spawn a new process here
+    forkFinally (clientThread conn) (const $ gracefulClose conn 5000)
+    network ip
+  where
+    -- -- interface
+    -- --   mv
+    -- --   ( atom $ do
+    -- --       x <- hReady handle
+    -- --       if x
+    -- --         then Just <$> hGetLine handle
+    -- --         else return Nothing
+    -- --   )
+    -- atom $ do
+    --   hClose handle
+    --   putStrLn "Socket closed."
+
+    resolve = do
+      let hints =
+            defaultHints
+              { addrFlags = [AI_PASSIVE],
+                addrSocketType = Stream
+              }
+      addrInfos <- getAddrInfo (Just hints) (Just ip) (Just "5000")
+      case addrInfos of
+        [] -> error "resolve returned no results"
+        (addrInfo : _) -> return addrInfo
+    open addr = do
+      sock <- socket (addrFamily addr) (addrSocketType addr) (addrProtocol addr)
+      setSocketOption sock ReuseAddr 1
+      withFdSocket sock setCloseOnExecIfNeeded
+      bind sock $ addrAddress addr
+      listen sock 1024
+      return sock
+
 main :: IO ()
 main = do
-  putStrLn "What port are you publishing over?"
-  port <- getLine
   putStrLn "What is your VPN IP address?"
   ip <- getLine
   -- create socket
-  (sa : _) <- getAddrInfo Nothing (Just ip) (Just port)
+  (sa : _) <- getAddrInfo Nothing (Just ip) (Just "5000")
   sock <- socket (addrFamily sa) Stream defaultProtocol
   connect sock (addrAddress sa)
-  -- loop
-  loop sock
-  where
-    loop sock =
-      listen
-        sock
-        1024
-        -- check for new connections + add to current list of members
-        -- move users around if need be
-        -- send messages between users
-        loop
-        sock
+  listen sock 1024
+
+-- loop
+
+data Action
+  = GetAllRooms
+  | GetAllRoomMessages RoomName
+  | CreateRoom RoomName
+  | -- | DeleteRoom RoomName
+    AddUserToRoom Username RoomName
+  | SwitchUserBetweenRooms Username RoomName RoomName
+  | SendRoomMessage Username RoomName MessageContent
+  deriving (Eq, Show)
+
+-- actionParser :: Parser Action
